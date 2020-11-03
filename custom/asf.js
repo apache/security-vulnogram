ddfde3bc70121302540fb6340a0f980eb13b297b
Mark J. Cox <mark@awe.com>
2020-11-03 13:20:17 +0000

    Move more custom stuff out of the way

--- custom/asf.js
+++ custom/asf.js
@@ -1,6 +1,25 @@
 const { v4: uuidv4 } = require('uuid');
 const request = require('request');
 const express = require('express');
+const conf = require('../config/conf');
+
+// If you are in security pmc allow you to specify a different pmc for testing
+
+function setpmc(req, res) {
+    if (req.isAuthenticated()) {
+	groups = req.user.pmcs;
+	if (groups.includes(conf.admingroupname)) {
+	    if (req.query.pmc) {
+		req.session.user.pmcs = req.query.pmc.split(',');
+		res.json({"result":"ok"});
+	    } else {
+		res.json({"error":"no pmc given"});
+	    }
+	} else {
+            res.json({"error":"you are not in "+conf.admingroupname+" pmc"});
+	}
+    }
+}
 
 function asflogin (req, res) {
     sess = req.session;
@@ -44,10 +63,11 @@ var self = module.exports = {
     },
 
     asfroutes: function (ensureAuthenticated, app) {
-        app.get("/users/login", function (req, res) { return asflogin(req,res) });
+        app.get("/users/login", asflogin);
         app.use('/.well-known', express.static("/home/mjc/server/.well-known", { dotfiles: 'allow' } ));
         let ac = require('../customRoutes/allocatecve');
         app.use('/allocatecve', ensureAuthenticated, ac.protected);
+        app.get('/users/setpmc', ensureAuthenticated, setpmc);
     },
     
 }
