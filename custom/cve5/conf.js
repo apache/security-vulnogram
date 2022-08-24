7049d692d140ea30799c352b86a8b08e9a484b99
Mark J. Cox <mark@awe.com>
2022-08-24 11:35:44 +0000

    Bit of a messy attempt at ASF customisation

--- custom/cve5/conf.js
+++ custom/cve5/conf.js
@@ -43,19 +43,67 @@ module.exports = {
                     "cna": {
                         "properties": {
                             "providerMetadata": { },
-                            "title": { },
-                            "datePublic": { },
+                            "title": {
+                                "options": {
+                                    "inputAttributes": {
+                                        "placeholder": "e.g. Apache Tomcat HTTP/2 denial of service (this is used for email subject)"
+                                    }
+                                }
+                            },
+                            "datePublic": {
+                                "options": {
+                                    "hidden": "true"
+                                }                                
+                            },
                             "problemTypes": { },
-                            "impacts": { },
+                            "impacts": {
+                                "options": {
+                                    "hidden": "true"
+                                }
+                            },
                             "source": {},
                             "affected": { },
-                            "descriptions": { },
-                            "references": { } ,
-                            "metrics": { },
+                            "descriptions": {
+				"title": "CVE Description: Please also include the product and version information in the description itself",       
+                                "items": {
+                                    "description": {
+                                        "title": "test1",
+                                    },
+                                    "options": {
+                                        "inputAttributes": {
+                                            "placeholder": "e.g. Apache Tomcat HTTP/2 denial of service (this is used for email subject)"
+                                        }                                        ,
+                                    },
+			            "properties": {
+				        "value": {
+				            "title": "description: Note that Mitre request that we please include the product and version information in the description itself as well as in the version section line in our submissions)",
+				            "description": "e.g. While investigating bug 60718, it was noticed that some calls to application listeners in Apache Tomcat versions 9.0.0.M1 to 9.0.0.M17, 8.5.0 to 8.5.11, 8.0.0.RC1 to 8.0.41 and 7.0.0 to 7.0.75 did not use the appropriate facade object. When running an untrusted application under a SecurityManager, it was therefore possible for that untrusted application to retain a reference to the request or response object and thereby access and/or modify information associated with another web application.",
+                                    "options": {
+                                        "inputAttributes": {
+                                            "placeholder": "e.g. Apache Tomcat HTTP/2 denial of service (this is used for email subject)"
+                                        }                                        ,
+                                    },
+                                            
+                                        }
+                                    }
+                                }
+                            },
+                            "references": {
+                                "title" : "references: use 'vendor-advisory' tag for pointer to ASF mailing list announcement",
+                                "options": {
+                                        "infoText": "test"
+                                }
+                            } ,
+                            "metrics": {
+                                "items": {
+                                    "required": ["other"],
+                                    "title": "Other" 
+                                }
+                            },
                             "configurations": { },
 	                    "workarounds": {
 		                "items": {
-                                    "title": "Mitigation/Work Around (optional, not sent to Mitre)",
+                                    "title": "Mitigation/Work Around (optional)",
                                 },                
 	                    },
                             "solutions": {},
