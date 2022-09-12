b6f7e03a3f653fc583861bef4aa4c5ca22837be5
Mark J. Cox <mark@awe.com>
2022-09-12 08:34:47 +0000

    Comment out the new change as it fails

--- default/cve5/preload.js
+++ default/cve5/preload.js
@@ -9,3 +9,18 @@ async function preloadCve() {
 preloadCve();
 if (document.getElementById('post1'))
     document.getElementById('post1').addEventListener('click', cvePost);
+console.log(docEditorOptions);
+console.log(docSchema);
+//var publicEditorOption = cloneJSON(docEditorOptions);
+//Object.assign(publicEditorOption.schema, docSchema.oneOf[0]);
+//delete publicEditorOption.schema.oneOf;
+
+//var rejectEditorOption = cloneJSON(docEditorOptions);
+//Object.assign(rejectEditorOption.schema, docSchema.oneOf[1]);
+//delete rejectEditorOption.schema.oneOf;
+
+//if(initJSON && initJSON.cveMetadata && initJSON.cveMetadata.state == 'REJECTED') {
+//    docEditorOptions = rejectEditorOption;
+//} else {
+//    docEditorOptions = publicEditorOption;
+//}
