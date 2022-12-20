fe05a701b0a1268f7e5b761830c690d8c4bae9f2
Mark J. Cox <mark@awe.com>
2022-12-20 13:50:40 +0000

    Grab the current document for the given CVE id

--- customRoutes/publishcve.js
+++ customRoutes/publishcve.js
@@ -4,14 +4,22 @@ const conf = require('../config/conf');
 const csurf = require('csurf');
 var request = require('request');
 const email = require('./email.js');
-//const doc = require('../routes/doc.js');
-//const optSet = require('../models/set');
-
+const asf =  require('../custom/asf.js');
 var csrfProtection = csurf();
 
 protected.post('/', csrfProtection, async function(req,res) {
-    console.log("publishcve", req.body);    
-    res.json({"body":"Push is not yet implemented. ("+req.body.cve+")"});
+    var q = {};
+    var opts = {"idpath":"body.cveMetadata.cveId"};
+    console.log("publishcve", req.body.cve);
+    q[opts.idpath] = req.body.cve;
+    let Document = res.locals.docs.cve5.Document;
+    var doc = await Document.findOne(q);
+    if (!doc) {
+        res.json({"body": req.body.cve+" not found"});
+        res.end();
+        return;
+    }
+    res.json({"body":"Push is not yet implemented. ("+req.body.cve+" "+doc.body.CNA_private.state+")"});
     res.end();    
     return true;
 });
