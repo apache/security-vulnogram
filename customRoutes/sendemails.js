3700467d55e9e179b988e8ddac28272971c50f94
Arnout Engelen <arnout@bzzt.net>
2026-05-04 11:37:42 +0200

    ASF: disable CVE and email in 'development' mode

--- customRoutes/sendemails.js
+++ customRoutes/sendemails.js
@@ -19,8 +19,10 @@ protected.get('/', csrfProtection, async function(req,res) {
 protected.post('/', csrfProtection, async function(req,res) {
     var to1 = req.body.emailto1;
     var to2 = req.body.emailto2;
-    var se1 = await email.sendemail({"from":"\""+req.user.name+"\" <"+req.user.email+">","to":to1,"replyTo":req.body.emailreplyto,"subject":req.body.emailsubject,"text":req.body.emailtext}).then( (x) => {  console.log("sent OSS notification mail "+x);});
-    var se2 = await email.sendemail({"from":"\""+req.user.name+"\" <"+req.user.email+">","to":to2,"bcc":"security@apache.org","replyTo":req.body.emailreplyto,"subject":req.body.emailsubject,"text":req.body.emailtext}).then( (x) => {  console.log("sent ASF notification mail "+x);});    
+    if (conf.cveapiliveservice) {
+        var se1 = await email.sendemail({"from":"\""+req.user.name+"\" <"+req.user.email+">","to":to1,"replyTo":req.body.emailreplyto,"subject":req.body.emailsubject,"text":req.body.emailtext}).then( (x) => {  console.log("sent OSS notification mail "+x);});
+        var se2 = await email.sendemail({"from":"\""+req.user.name+"\" <"+req.user.email+">","to":to2,"bcc":"security@apache.org","replyTo":req.body.emailreplyto,"subject":req.body.emailsubject,"text":req.body.emailtext}).then( (x) => {  console.log("sent ASF notification mail "+x);});    
+    }
     req.flash('error','Sent the emails!');
     res.render('blank');
     res.end();
